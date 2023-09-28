e11c590c9a6038bed39a4109fdb87ad96b0f71f6
Lucas Cubile <cubilelucas@gmail.com>
2023-09-28 14:47:31 -0300

    two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-first-angular-app';
+
+  changeTitle() {
+    this.title = 'The title has been changed';
+  }
+
+  changeTitleBack() {
+    this.title = 'my-first-angular-app';
+  }
 }
